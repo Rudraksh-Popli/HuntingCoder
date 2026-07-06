ba86f3e5db4c946b1488cda252df0e8e898d847e
Rudraksh-Popli <poplirudraksh00@gmail.com>
2026-07-06 13:25:12 +0530

    added blog jsons as data

--- pages/blog.js
+++ pages/blog.js
@@ -8,23 +8,23 @@ const Blog = () => {
             <main className={styles.main}>
                 <div className={styles.blogItem}>
                     <Link href={'blogpost/learn-javascript'}>
-                        <h3>How to Learn JavaScript in 2026</h3>
+                        <h2>How to Learn JavaScript in 2026</h2>
                     </Link>
                     <br />
                     <p>JavaScript is the language used to design logic for the web</p>
                 </div>
                 <div className={styles.blogItem}>
-                    <h3>How to Learn JavaScript in 2026</h3>
+                    <h2>How to Learn JavaScript in 2026</h2>
                     <br />
                     <p>JavaScript is the language used to design logic for the web</p>
                 </div>
                 <div className={styles.blogItem}>
-                    <h3>How to Learn JavaScript in 2026</h3>
+                    <h2>How to Learn JavaScript in 2026</h2>
                     <br />
                     <p>JavaScript is the language used to design logic for the web</p>
                 </div>
                 <div className={styles.blogItem}>
-                    <h3>How to Learn JavaScript in 2026</h3>
+                    <h2>How to Learn JavaScript in 2026</h2>
                     <br />
                     <p>JavaScript is the language used to design logic for the web</p>
                 </div>
