d56c51f06f0e8138c3034335397c444a44c655c2
Rudraksh-Popli <poplirudraksh00@gmail.com>
2026-07-09 15:43:45 +0530

    populated blogpost using getblog endpoint

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import styles from "@/styles/BlogPost.module.css";
 
 const slug = () => {
     const router = useRouter();
-    const { slug } = router.query;
+    const [blog, setBlog] = useState();
+    useEffect(() => {
+        if (!router.isReady) {
+            return;
+        }
+        const { slug } = router.query;
+        fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((fetchedData) => {
+            return fetchedData.json();
+        }).then((data) => {
+            setBlog(data);
+        })
+    }, [router.isReady])
     return (
         <div className={styles.container}>
             <main className={styles.main}>
-                <h1>Title of the page {slug}</h1>
+                <h1>{blog && blog.title}</h1>
                 <br /><br />
                 <div>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque exercitationem eius natus ab id sequi vero itaque praesentium earum, quam velit doloremque molestiae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, distinctio voluptate itaque aspernatur aut at quaerat natus. Illo fugit odio incidunt doloribus quia non unde enim facere ipsum a dolor doloremque, placeat quisquam? Consequuntur, veniam? Deserunt officiis vitae nobis laudantium ipsum similique. Sapiente, placeat earum!
+                    {blog && blog.content}
                 </div>
             </main>
         </div>
