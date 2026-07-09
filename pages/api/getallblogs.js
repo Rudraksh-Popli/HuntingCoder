31af40a10259623009727f6174ddafc325480988
Rudraksh-Popli <poplirudraksh00@gmail.com>
2026-07-09 13:38:58 +0530

    used API endpoint

--- pages/api/getallblogs.js
+++ pages/api/getallblogs.js
@@ -2,11 +2,14 @@
 
 import fs from 'node:fs';
 
-export default function handler(req, res) {
-    fs.readdir("blogdata", (err, data) => {
-        if (err) {
-            res.status(500).json({ error: "No Blogs Found" });
-        }
-        res.status(200).json(data);
-    })
+export default async function handler(req, res) {
+    let data = await fs.promises.readdir("blogdata");
+    let myBlog;
+    let allBlogs = [];
+    for (let index = 0; index < data.length; index++) {
+        const element = data[index];
+        myBlog = await fs.promises.readFile(`blogdata/${element}`, 'utf-8');
+        allBlogs.push(JSON.parse(myBlog));
+    }
+    res.status(200).json(allBlogs);
 }
--- pages/blog.js
+++ pages/blog.js
@@ -1,33 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from "@/styles/Blog.module.css";
 import Link from 'next/link';
 
 const Blog = () => {
+    const [blogs, setBlogs] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:3000/api/getallblogs").then((fetchedData) => {
+            return fetchedData.json();
+        }).then((data) => {
+            setBlogs(data);
+        })
+    }, [])
     return (
         <div className={styles.container}>
             <main className={styles.main}>
-                <div className={styles.blogItem}>
-                    <Link href={'blogpost/learn-javascript'}>
-                        <h2>How to Learn JavaScript in 2026</h2>
-                    </Link>
-                    <br />
-                    <p>JavaScript is the language used to design logic for the web</p>
-                </div>
-                <div className={styles.blogItem}>
-                    <h2>How to Learn JavaScript in 2026</h2>
-                    <br />
-                    <p>JavaScript is the language used to design logic for the web</p>
-                </div>
-                <div className={styles.blogItem}>
-                    <h2>How to Learn JavaScript in 2026</h2>
-                    <br />
-                    <p>JavaScript is the language used to design logic for the web</p>
-                </div>
-                <div className={styles.blogItem}>
-                    <h2>How to Learn JavaScript in 2026</h2>
-                    <br />
-                    <p>JavaScript is the language used to design logic for the web</p>
-                </div>
+                {blogs.map((blog_item) => {
+                    return (
+                        <div key={blog_item.slug} className={styles.blogItem}>
+                            <Link href={`blogpost/${blog_item.slug}`}>
+                                <h2>{blog_item.title}</h2>
+                            </Link>
+                            <br />
+                            <p>{blog_item.content.substr(0, 200)}...</p>
+                        </div>
+                    )
+                })}
             </main>
         </div>
     )
