59569861dbc26fe6ba9f2fff0db72d29ff0e9ae0
Rudraksh-Popli <poplirudraksh00@gmail.com>
2026-07-10 18:56:38 +0530

    performed server side rendering on blogpost page

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -2,20 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import styles from "@/styles/BlogPost.module.css";
 
-const slug = () => {
-    const router = useRouter();
-    const [blog, setBlog] = useState();
-    useEffect(() => {
-        if (!router.isReady) {
-            return;
-        }
-        const { slug } = router.query;
-        fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((fetchedData) => {
-            return fetchedData.json();
-        }).then((data) => {
-            setBlog(data);
-        })
-    }, [router.isReady])
+const slug = (props) => {
+    const [blog, setBlog] = useState(props.myBlog);
     return (
         <div className={styles.container}>
             <main className={styles.main}>
@@ -28,5 +16,14 @@ const slug = () => {
         </div>
     )
 };
+
+export async function getServerSideProps(context) {
+    const { slug } = context.query;
+    let data = await fetch(`http://localhost:3000/api/getblog?slug=${slug}`);
+    let myBlog = await data.json();
+    return {
+        props: { myBlog },
+    }
+}
 
 export default slug;
