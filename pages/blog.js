692d114b8d99e8a1122e8733e04dcca5f429e6b1
Rudraksh-Popli <poplirudraksh00@gmail.com>
2026-07-10 18:56:13 +0530

    removed redundant console log statement

--- pages/blog.js
+++ pages/blog.js
@@ -4,7 +4,6 @@ import Link from 'next/link';
 
 const Blog = (props) => {
     const [blogs, setBlogs] = useState(props.allBlogs);
-    console.log(props);
     return (
         <div className={styles.container}>
             <main className={styles.main}>
